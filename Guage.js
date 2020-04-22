385338a6387f5bc604caca6a570034d61fc1bf60
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2020-04-22 17:58:09 +0530

    Update Guage.js

--- Guage.js
+++ Guage.js
@@ -360,7 +360,7 @@ google.v.w.H.Cn=function(b){if(typeof b===I)try{if(""!==b)for(var c=JSON.parse(b
 
         var data = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
-          [props.label, props.value],
+          [myprops.label, myprops.value],
           
         ]);
 
