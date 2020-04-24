b195a9a197500dd36dbe32de27aac6bb7999285c
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2020-04-24 11:06:21 +0530

    Update Guage.js

--- Guage.js
+++ Guage.js
@@ -2,6 +2,32 @@
     let template = document.createElement("template");
     template.innerHTML = `
 		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script type="text/javascript">
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Memory', 80],
+          ['CPU', 55],
+          ['Network', 68]
+        ]);
+
+        var options = {
+          width: 400, height: 120,
+          redFrom: 90, redTo: 100,
+          yellowFrom:75, yellowTo: 90,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+
+            }
+    </script>
         <style>
         :host {
             border-radius: 25px;
