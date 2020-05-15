f94113064ee304c5c66f684cfb6bf5ecff349602
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2020-05-15 16:59:29 +0530

    Update Guage.js

--- Guage.js
+++ Guage.js
@@ -23,52 +23,17 @@
 			      this._props = {};
 		    }
 	    
-        render(val) {
-            if(!gLibLoaded){
+        function () 
+		 {
+ 
                 const script = document.createElement('script');
                 script.type = 'text/javascript';
                 script.async = true;
-                script.onload = function () {
-                    gLibLoaded = true;
-                    
-                    if(val!==''){
-                  
-                        google.charts.load('current', {'packages':['gauge']});
-                        google.charts.setOnLoadCallback(function() {
-                            drawChart(val);
-                        });
-            
-                        function drawChart(props) {
-                            
-                            var data = google.visualization.arrayToDataTable([
-                            ['Label', 'Value'],
-                            [props.label, props.value]
-                            ]);
-            
-                            var options = {
-                            chartArea: {
-                            
-                                width: '94%'
-                                },
-                                legend: {
-                                position: 'top'
-                                },
-                                width: '100%',
-                            redFrom: props.redFrom, redTo: props.redTo,
-                            yellowFrom:props.yellowFrom, yellowTo: props.yellowTo,
-                            minorTicks: 5
-                            };
-                            const ctx = document.querySelector(".sapCustomWidgetWebComponent").shadowRoot.querySelector("#chart_div");
-                                                                
-                            var chart = new google.visualization.Gauge(ctx);
-                            chart.draw(data, options);
-                        }                           
-                    }
-                }
+
                 script.src = 'https://www.gstatic.com/charts/loader.js';
                 //Append it to the document header
                 document.head.appendChild(script);
-            }
+            
 	    }
 
         onCustomWidgetBeforeUpdate(changedProperties) {
@@ -77,8 +42,7 @@
 
         onCustomWidgetAfterUpdate(changedProperties) {
             this._props = { ...this._props, ...changedProperties };
-            var myprops = this._props
-	        this.render(myprops);
+           
 	    	
         }
     }
