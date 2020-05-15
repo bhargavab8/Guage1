8793a8ee21d502eaa23ea455ba267732029fc061
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2020-05-15 18:07:12 +0530

    Update Guage.js

--- Guage.js
+++ Guage.js
@@ -8,8 +8,6 @@
   </style> 
 <div id="chart_div"></div>`;
     
-    let gLibLoaded = false;
-    
     class GoogleGauge extends HTMLElement {
 		    constructor() {
 			      super(); 
@@ -23,54 +21,6 @@
 			      this._props = {};
 		    }
 	    
-        render(val) {
-            if(!gLibLoaded){
-                const script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.async = true;
-                script.onload = function () {
-                    gLibLoaded = true;
-                    
-                    if(val!==''){
-                  
-                        google.charts.load('current', {'packages':['gauge']});
-                        google.charts.setOnLoadCallback(drawChart);
-                        function drawChart() {                                                
-                                var data = google.visualization.arrayToDataTable([
-                                ['Label', 'Value'],
-                                [val.label, val.value]
-                                ]);
-                
-                                var options = {
-                                chartArea: {
-                                
-                                    width: '94%'
-                                    },
-                                    legend: {
-                                    position: 'top'
-                                    },
-                                    width: '100%',
-                                redFrom: val.redFrom, redTo: val.redTo,
-                                yellowFrom:val.yellowFrom, yellowTo: val.yellowTo,
-                                minorTicks: 5
-                                };
-                                const ctx = document.querySelector(".sapCustomWidgetWebComponent").shadowRoot.querySelector("#chart_div");
-                                                                    
-                                var chart = new google.visualization.Gauge(ctx);
-                                chart.draw(data, options);
-                            
-
-                        };
-            
-                                                  
-                    }
-                }
-                script.src = 'https://www.gstatic.com/charts/loader.js';
-                //Append it to the document header
-                document.head.appendChild(script);
-            }
-	    }
-
         onCustomWidgetBeforeUpdate(changedProperties) {
             this._props = { ...this._props, ...changedProperties };
         }
@@ -78,9 +28,44 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             this._props = { ...this._props, ...changedProperties };
             var myprops = this._props
-	        this.render(myprops);
-	    	
-        }
+			
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.async = true;
+			script.onload = function () {                  
+					google.charts.load('current', {'packages':['gauge']});
+					google.charts.setOnLoadCallback(function(){
+						drawChart(myprops);
+					});
+					function drawChart(props) {                                                
+							var data = google.visualization.arrayToDataTable([
+							['Label', 'Value'],
+							[props.label, props.value]
+							]);
+			
+							var options = {
+							chartArea: {
+							
+								width: '94%'
+								},
+								legend: {
+								position: 'top'
+								},
+								width: '100%',
+							redFrom: props.redFrom, redTo: props.redTo,
+							yellowFrom:props.yellowFrom, yellowTo: props.yellowTo,
+							minorTicks: 5
+							};
+							const ctx = document.querySelector(".sapCustomWidgetWebComponent").shadowRoot.querySelector("#chart_div");
+																
+							var chart = new google.visualization.Gauge(ctx);
+							chart.draw(data, options);
+							};                                                            
+				}
+			script.src = 'https://www.gstatic.com/charts/loader.js';
+			//Append it to the document header
+			document.head.appendChild(script);
+            }
     }
     customElements.define("com-sample-guage", GoogleGauge);
 })();
